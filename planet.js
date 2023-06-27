c2bad56c37d210df3d41c96300d4d16ce02d4128
Alice Smith <alice@smith.com>
2023-06-27 11:00:39 -0400

    Display films with planets

---
+++ planet.js
@@ -0,0 +1,67 @@
+let nameH1;
+let birthYearSpan;
+let heightSpan;
+let massSpan;
+let filmsDiv;
+let planetDiv;
+const baseUrl = `https://swapi2.azurewebsites.net/api`;
+
+// Runs on page load
+addEventListener('DOMContentLoaded', () => {
+  nameH1 = document.querySelector('h1#name');
+//   birthYearSpan = document.querySelector('span#birth_year');
+//   massSpan = document.querySelector('span#mass');
+//   heightSpan = document.querySelector('span#height');
+//   homeworldSpan = document.querySelector('span#homeworld');
+  filmsUl = document.querySelector('#films>ul');
+  const sp = new URLSearchParams(window.location.search)
+  const id = sp.get('id')
+  getPlanet(id)
+});
+
+async function getPlanet(id) {
+  let planet;
+  console.log(id)
+  try {
+    planet = await fetchPlanet(id)
+    // planet.homeworld = await fetchHomeworld(character)
+    planet.films = await fetchFilms(planet)
+    console.log("Planet",planet)
+  }
+  catch (ex) {
+    console.error(`Error reading character ${id} data.`, ex.message);
+  }
+  renderPlanet(planet);
+
+}
+
+async function fetchPlanet(id) {
+  let planetUrl = `${baseUrl}/planet/${id}`;
+  return await fetch(planetUrl)
+    .then(res => res.json())
+}
+
+// async function fetchHomeworld(character) {
+//   const url = `${baseUrl}/planets/${character?.homeworld}`;
+//   const planet = await fetch(url)
+//     .then(res => res.json())
+//   return planet;
+// }
+
+async function fetchFilms(planet) {
+  const url = `${baseUrl}/planet/${planet?.id}/films`;
+  const films = await fetch(url)
+    .then(res => res.json())
+  return films;
+}
+
+const renderPlanet = planet => {
+//   document.title = `SWAPI - ${character?.name}`;  // Just to make the browser tab say their name
+//   nameH1.textContent = character?.name;
+//   heightSpan.textContent = character?.height;
+//   massSpan.textContent = character?.mass;
+//   birthYearSpan.textContent = character?.birth_year;
+//   homeworldSpan.innerHTML = `<a href="/planet.html?id=${character?.homeworld.id}">${character?.homeworld.name}</a>`;
+  const filmsLis = planet?.films?.map(film => `<li><a href="/film.html?id=${film.id}">${film.title}</li>`)
+  filmsUl.innerHTML = filmsLis.join("");
+}
